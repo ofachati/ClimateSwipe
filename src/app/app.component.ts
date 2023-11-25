50698bcdd7a292e61c179d21e114f6601c7450f0
Omar EL FACHATI <elfachatiomar@gmail.com>
2023-11-25 16:34:11 +0100

    improve landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('slideIn', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)' }),
+        animate('500ms ease-in', style({ transform: 'translateY(0%)' })),
+      ]),
+    ]),
+  ],
 })
-export class AppComponent {
+
+export class AppComponent{
   title = 'nuit_info_front';
+  showSinger: boolean = false;
+
+  displayEasterEgg = false;
+  clickCount = 0;
+
+
+
+
+  onPress() {
+    this.clickCount = ++this.clickCount;
+    if (this.clickCount == 6) {
+      this.displayEasterEgg = true;
+      this.clickCount = 0;
+    }
+  }
+
+  
+  triggerEasterEgg() {
+    const audioElement = document.getElementById('rickRollAudio') as HTMLAudioElement;
+
+    // Play the music only if it's not already playing
+    if (audioElement.paused) {
+      audioElement.play();
+
+      // Show the sliding animation
+      this.showSinger = true;
+
+      // Stop the audio after 10 seconds (adjust the duration as needed)
+      setTimeout(() => {
+        audioElement.pause();
+        audioElement.currentTime = 0;
+        this.showSinger = false;
+      }, 9000); //9 seconds in milliseconds
+    }
+  }
+
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatGridListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTooltipModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
