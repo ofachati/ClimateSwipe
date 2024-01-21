5e158902f7ca313e3d622d51721ba7672be7bda9
Omar EL FACHATI <elfachatiomar@gmail.com>
2024-01-21 18:40:08 +0100

    Add AI component to app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AnalyticComponent } from './components/analytic/analytic.component';
+import { AiComponent } from './components/ai/ai.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent, data: { title: 'Home' } },
   { path: 'analytic', component: AnalyticComponent, data: { title: 'Analytics' } },
+  { path: 'ai', component: AiComponent, data: { title: 'Ai' } },
   { path: '**', component: NotFoundComponent },
 ];
 
---
+++ src/app/components/ai/ai.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ai',
+  standalone: true,
+  imports: [],
+  templateUrl: './ai.component.html',
+  styleUrl: './ai.component.scss'
+})
+export class AiComponent {
+
+}
